34448aca4b832303366551bbb8228936023fa177
LEEMYOENGJE <djroehd@naver.com>
2023-10-20 17:25:37 +0900

    로그인 서비스 구현(1)

--- src/test/java/models/member/LoginService.java
+++ src/test/java/models/member/LoginService.java
@@ -4,6 +4,17 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class LoginService {
     public void login(HttpServletRequest request) {
+    }
 
+    private Member getMember() {
+        String userPw = "12345678";
+        return Member.builder()
+                .userId("user" + System.currentTimeMillis())
+                .userPw(userPw)
+                .confirmUserPw(userPw)
+                .userNm("사용자")
+                .email("user@test.org")
+                .agree(true)
+                .build();
     }
 }
